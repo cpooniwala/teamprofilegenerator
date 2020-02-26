ae618341158a6933cfc46373c402f80f812d8007
cpooniwala <cpooniwala@gmail.com>
2020-02-26 11:19:15 -0600

    created user input

--- app.js
+++ app.js
@@ -1,15 +1,93 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-const questions = [
+const employeeRole = {
+  type: "list",
+  name: "role",
+  message: "What employee type are you creating?",
+  choices: ["Employee", "Manager", "Engineer", "Intern"]
+};
+
+const employee = [
   {
-    message: "Enter your GitHub username",
-    name: "username"
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your id?"
+  }
+];
+
+const manager = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your id?"
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your office number?"
+  }
+];
+
+const engineer = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your id?"
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?"
+  }
+];
+
+const intern = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your id?"
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school do you attend?"
   }
 ];
 
 function init() {
-  inquirer.prompt(questions);
+  inquirer.prompt(employeeRole).then(employeeRole => {
+    console.log(employeeRole.role);
+    if (employeeRole.role === "Employee") {
+      inquirer.prompt(employee);
+    } else if (employeeRole.role === "Manager") {
+      inquirer.prompt(manager);
+    } else if (employeeRole.role === "Engineer") {
+      inquirer.prompt(engineer);
+    } else {
+      inquirer.prompt(intern);
+    }
+  });
 }
 
 init();
